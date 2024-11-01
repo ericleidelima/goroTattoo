72d4d4bfbebef33116c30802187dd9f169ee2bf9
Ericlei de lima <ericleilima@gmail.com>
2024-10-31 23:50:54 -0300

    inserimos uma pagina JS para controlar umas funções

---
+++ control.js
@@ -0,0 +1,29 @@
+function espelharDiv(divId){
+      
+    const divToMirror = document.getElementById(divId);
+    
+    const container2 = document.getElementById('containerX');
+
+    container2.innerHTML = divToMirror.innerHTML;
+
+    esconderImagem2()
+  };
+
+
+  function mostrarDiv() {
+            const div1 = window.document.getElementById("divInvisivel");
+            
+            if (div1.style.display === "none" ) {
+        
+                div1.style.display = "block"; 
+            } else {
+                div1.style.display = "none";
+            }
+           
+        };
+
+
+function esconderImagem2() {
+            var imagem2 = document.getElementById("divInvisivel");
+            imagem2.style.display = "none"; // Esconde a imagem2
+        };
